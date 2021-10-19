e487df6c4dd9cd1b03cf38114345730025d8106a
mufkhalif <mufkalif18@gmail.com>
2021-10-19 17:01:58 +0700

    main build

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:food_app/pages/home_movie.dart';
 import 'package:get/route_manager.dart';
 
-// add comment
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
